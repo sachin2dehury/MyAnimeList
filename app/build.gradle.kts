98c63f8cb170f2747513d7d08128e82d06a2dc5f
Sachin Dehury <51115041+sachin2dehury@users.noreply.github.com>
2023-12-06 17:47:05 +0530

    ci fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
